e94b9d5b3b832be54adfdd33b515d336877fe93b
Asset <asset.aktay@gmail.com>
2020-03-01 11:54:35 +0600

    # add angular material # node-sass

---
+++ src/app/shared/material/material.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+const UI_COMPONENTS = [];
+
+@NgModule({
+  declarations: [],
+  imports: [ CommonModule, UI_COMPONENTS ],
+  exports: [ UI_COMPONENTS ]
+})
+export class MaterialModule { }
